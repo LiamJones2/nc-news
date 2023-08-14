36c18bd60e7bb1ff598b4040ffa137d435967ee1
liamjones2 <liammjoness@gmail.com>
2023-08-14 15:23:22 +0100

    nc-news-4 Added articles controller with tests and modified endpoints.json

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,64 +10,79 @@ afterAll(() => db.end());
 
 describe('Test GET /api endpoint', () => {
   test('Test connection with GET /api, should receive 200 as the status code to show connection', () => {
-      return request(app)
-          .get('/api')
-          .expect(200)                  
+    return request(app)
+      .get('/api')
+      .expect(200)
   })
   test('Test connection with GET /api, should receive all /api showing all endpoints', () => {
-      return request(app)
-          .get('/api')
-          .expect(200)
-          .then(({body}) => {
-              expect(body).toEqual(endpoints)
-          })        
+    return request(app)
+      .get('/api')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual(endpoints)
+      })
   })
 })
 
 describe('Test GET api/topics  endpoint', () => {
-    test('Test connection with GET api/topics, should receive 200 as the status code to show connection', () => {
-        return request(app)
-            .get('/api/topics')
-            .expect(200)                  
-    })
-    test('Test connection with GET api/topics, should receive all topics which should have keys (slug and description)', () => {
-        return request(app)
-            .get('/api/topics')
-            .expect(200)
-            .then(({body}) => {
-                expect(body.length > 0).toBe(true)
-                expect(body).toEqual([{"description": "The man, the Mitch, the legend", "slug": "mitch"}, 
-                  {"description": "Not dogs", "slug": "cats"}, 
-                  {"description": "what books are made of", "slug": "paper"}
-                ])
-            })        
-    })
+  test('Test connection with GET api/topics, should receive 200 as the status code to show connection', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+  })
+  test('Test connection with GET api/topics, should receive all topics which should have keys (slug and description)', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length > 0).toBe(true)
+        expect(body).toEqual([{ "description": "The man, the Mitch, the legend", "slug": "mitch" },
+        { "description": "Not dogs", "slug": "cats" },
+        { "description": "what books are made of", "slug": "paper" }
+        ])
+      })
+  })
 })
 
 describe('Test GET api/articles  endpoint', () => {
   test('Test connection with GET api/articles, should receive 200 as the status code to show connection', () => {
-      return request(app)
-          .get('/api/articles/1')
-          .expect(200)                  
+    return request(app)
+      .get('/api/articles/1')
+      .expect(200)
   })
   test('Test connection with GET api/articles, should receive all topics which should have keys (slug and description)', () => {
-      return request(app)
-          .get('/api/articles/1')
-          .expect(200)
-          .then(({body}) => {
-              expect(body.length > 0).toBe(true) 
-              console.log(body)
-              expect(body).toEqual([{
-                article_id: 1,
-                title: "Living in the shadow of a great man",
-                topic: "mitch",
-                author: "butter_bridge",
-                body: "I find this existence challenging",
-                created_at: "2020-07-09T20:11:00.000Z",
-                votes: 100,
-                article_img_url:
-                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-              }])
-          })        
+    return request(app)
+      .get('/api/articles/1')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length > 0).toBe(true)
+        expect(body).toEqual([{
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        }])
+      })
+  })
+  test('Test that article id requested does not exist causing a Bad Request 400 response when given a number', () => {
+    return request(app)
+      .get('/api/articles/999999')
+      .expect(400)
+      .then(({ body }) => {
+        expect(body).toEqual({ msg: "Bad Request" })
+      })
+  })
+  test('Test that article id requested does not exist causing a Bad Request 400 response when given a random string', () => {
+    return request(app)
+      .get('/api/articles/banana')
+      .expect(400)
+      .then(({ body }) => {
+        expect(body).toEqual({ msg: "Bad Request" })
+      })
   })
 })
--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticles);
 
 app.use((err, req, res, next) => {
+    if(err.message === "Bad Request" || err.code === '22P02'){
+        res.status(400).send({msg:"Bad Request"})
+    }
+})
+
+app.use((err, req, res, next) => {
     res.status(500).send({msg:"Server is unable to access data at the moment"})
 })
 

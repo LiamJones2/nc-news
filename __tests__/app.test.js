708bcf0b9614102c8e284dab3aedb2d87196fa33
liamjones2 <liammjoness@gmail.com>
2023-08-14 12:30:28 +0100

    Completed requested changes as requested in previous pull request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,30 +2,27 @@ const app = require('../app');
 const request = require('supertest');
 const db = require('../db/connection.js');
 const seed = require('../db/seeds/seed.js');
-const { topicData, userData, articleData, commentData } = require('../db/data/test-data/');
+const testData = require('../db/data/test-data/');
 
-beforeEach(() => seed({ topicData, userData, articleData, commentData }));
+beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe('Test /api/topics endpoint', () => {
-    test('Test connection with /api/topics, should receive 200 as the status code to show connection', () => {
+describe('Test GET api/topics  endpoint', () => {
+    test('Test connection with GET api/topics, should receive 200 as the status code to show connection', () => {
         return request(app)
             .get('/api/topics')
             .expect(200)                  
     })
-    test('Test connection with /api/topics, should receive all topics which should have keys (slug and description)', () => {
+    test('Test connection with GET api/topics, should receive all topics which should have keys (slug and description)', () => {
         return request(app)
             .get('/api/topics')
             .expect(200)
             .then(({body}) => {
-                body.forEach((topic) => {
-                    expect(Object.keys(topic)).toEqual(
-                      expect.arrayContaining([
-                        "slug",
-                        "description"
-                      ])
-                    );
-                  });
+                expect(body.length > 0).toBe(true)
+                expect(body).toEqual([{"description": "The man, the Mitch, the legend", "slug": "mitch"}, 
+                  {"description": "Not dogs", "slug": "cats"}, 
+                  {"description": "what books are made of", "slug": "paper"}
+                ])
             })        
     })
 })
--- models/topics.model.js
+++ models/topics.model.js
@@ -4,7 +4,4 @@ exports.returnTopics = () => {
     return db.query('SELECT * FROM topics').then(({rows}) => {
         return rows
     })
-    .catch((err) => {
-        throw err
-    })
 }

a1357d2db344d45be5897173b0fad79d913985d6
liamjones2 <liammjoness@gmail.com>
2023-08-17 11:56:18 +0100

    nc-news-9 changes

--- models/comments.model.js
+++ models/comments.model.js
@@ -16,7 +16,6 @@ exports.returnAllCommentsByArticleId = (article_id) => {
 
 
 exports.addNewCommentByArticleIdToDatabase = (article_id, username, body) => {
-    article_id = Number(article_id)
     if (username === undefined || body === undefined) return Promise.reject({ status: 400, msg: "Bad Request" })
     const checkForArticle = db.query('SELECT * FROM articles WHERE article_id = $1', [article_id])
     const checkForUsername = db.query('SELECT * FROM users WHERE username = $1', [username])
@@ -37,15 +36,10 @@ exports.addNewCommentByArticleIdToDatabase = (article_id, username, body) => {
 
 
 exports.deleteCommentFromDatabase = (comment_id) => {
-    comment_id = Number(comment_id)
-    return db.query('SELECT * FROM comments WHERE comment_id = $1', [comment_id])
-        .then((comment) => {
-            if (comment.rows.length === 0) return Promise.reject({ status: 404, msg: "Comment Not Found" })
-            else {
-                return db.query('DELETE FROM comments WHERE comment_id = $1;', [comment_id])
-                    .then(() => {
-                        return
-                    })
-            }
+    return db.query('DELETE FROM comments WHERE comment_id = $1;', [comment_id])
+        .then(({rowCount}) => {           
+            if(rowCount === 0 ) return Promise.reject({status:404, msg:"Comment Not Found"})
+            return
         })
 }
+

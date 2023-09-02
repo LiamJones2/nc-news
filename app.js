35dbce501cf56f76cfe464929a97f642f6eb2b5c
liamjones2 <liammjoness@gmail.com>
2023-09-02 15:39:34 +0100

    Fixed access-control-allow-origin all

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const commentsRouter = require('./routes/comments-router.js');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+
+    next();
+});
+
 app.use('/api', apiRouter);
 
 app.use('/api/articles', articlesRouter);
